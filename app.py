758825aa536bdc3433d98b7c488c4c40d4068314
hm <hm.tuomaala@gmail.com>
2021-01-27 17:07:02 +0200

    Add documentation

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ app = Flask(__name__)
 API_BASE = "https://bad-api-assignment.reaktor.com"
 
 def find_availability(id, man):
+    """
+    Parameters
+    ----------
+    id: str
+        id of the product
+    man: str
+        manufacturer of the product
+
+    Returns
+    -------
+    str
+        returns the availability value of the product
+    """
     global MANUFACTURER_AVAILABILITY
     for item in MANUFACTURER_AVAILABILITY[man]:
         if item["id"] == id:
@@ -18,11 +31,13 @@ def find_availability(id, man):
 
 @app.route("/", methods = ['GET'])
 def index():
+    """Returns the index page"""
     return render_template("index.html")
 
 
 @app.route("/gloves", methods = ['GET'])
 def gloves():
+    """Returns the page for the gloves"""
     r = requests.get(API_BASE + "/v2/products/gloves")
     if r.status_code != 200:
         return "ERROR -- Try reloading the page"
@@ -33,6 +48,7 @@ def gloves():
 
 @app.route("/facemasks", methods = ['GET'])
 def facemasks():
+    """Returns the page for the facemasks"""
     r = requests.get(API_BASE + "/v2/products/facemasks")
     if r.status_code != 200:
         return "ERROR -- Try reloading the page"
@@ -43,6 +59,7 @@ def facemasks():
 
 @app.route("/beanies", methods = ['GET'])
 def beanies():
+    """Returns the page for the beanies"""
     r = requests.get(API_BASE + "/v2/products/beanies")
     if r.status_code != 200:
         return "ERROR -- Try reloading the page"
@@ -53,6 +70,22 @@ def beanies():
 
 @app.route('/background_process/<id>/<manufacturer>', methods = ["GET"])
 def background_process(id, manufacturer):
+    """
+    Searches availability info for the requested product.
+
+    Parameters
+    ----------
+    id: str
+        id of the product
+    manufacturer: str
+        manufacturer of the product
+
+    Returns
+    -------
+    str
+        returns the availability value of the product or 'ERROR' if the API
+        request was falty.
+    """
     id = id.upper()
 
     global MANUFACTURER_AVAILABILITY
