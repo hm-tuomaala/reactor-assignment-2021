58e2338dc9a39cee9147e7e729c09a28597c3315
hm <hm.tuomaala@gmail.com>
2021-01-27 11:20:40 +0200

    Availability functionality works

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask, request, render_template
 import xml.etree.ElementTree as ET
 import requests
 import time
+from globals import MANUFACTURER_AVAILABILITY
 
 app = Flask(__name__)
 
 API_BASE = "https://bad-api-assignment.reaktor.com"
+
+def find_availability(id, man):
+    global MANUFACTURER_AVAILABILITY
+    for item in MANUFACTURER_AVAILABILITY[man]:
+        if item["id"] == id:
+            return ET.fromstring(item["DATAPAYLOAD"]).find("INSTOCKVALUE").text
+    return "NOT FOUND"
 
 @app.route("/", methods = ['GET'])
 def index():
@@ -22,35 +30,29 @@ def gloves():
         return "ERROR"
     data = r.json()
 
-    # for item in data:
-    #     makers.add(item["manufacturer"])
-    #
-    # for maker in makers:
-    #     if maker not in MA.keys():
-    #         req = requests.get(API_BASE + "/v2/availability/" + maker)
-    #         MA[maker] = {}
-    #         for prod in req.json()["response"]:
-    #             MA[maker][prod["id"]] = prod["DATAPAYLOAD"]
-    #
-    # for item in data:
-    #     product = item
-    #     product["availability"] = ET.fromstring(MA[product["manufacturer"]][product["id"]]).find("INSTOCKVALUE").text
-    #     ret_data.append(item)
-
-
     return render_template("gloves.html", items = data)
 
 
 @app.route('/background_process_test/<id>/<manufacturer>')
 def background_process_test(id, manufacturer):
-    print(id, manufacturer)
+    print(id.upper(), manufacturer)
+    id = id.upper()
+
+    global MANUFACTURER_AVAILABILITY
+    if manufacturer in MANUFACTURER_AVAILABILITY.keys():
+        print("GLOBAL")
+        return find_availability(id, manufacturer)
+        print("GLOBAL FAILED")
+
+    print("MAKING REQUEST")
     req = requests.get(API_BASE + "/v2/availability/" + manufacturer)
-    if req.status_code != 200:
+
+    if req.status_code != 200 or len(req.json()["response"]) <= 2:
         return "ERROR"
-    for prod in req.json()["response"]:
-        if prod["id"] == id:
-            return ET.fromstring(prod["DATAPAYLOAD"]).find("INSTOCKVALUE").text
-    return "ERROR"
+
+    MANUFACTURER_AVAILABILITY[manufacturer] = req.json()["response"]
+
+    return find_availability(id, manufacturer)
 
 
 @app.route("/facemasks", methods = ['GET'])
